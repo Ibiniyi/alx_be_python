80ab3ac150310d533d705e6d57108f109779384a
Timothy <timothyniyi@gmail.com>
2025-09-08 19:32:49 -0700

    variable assign

--- python_introduction/rectangle_area.py
+++ python_introduction/rectangle_area.py
@@ -1,4 +1,7 @@
-length=10
-width=5
+#variable declearation
+length= 10
+width= 5
+#formula for the area of triangle
 area= length*width
+#output the value of area 
 print(f"the area of the rectangle is: {area}")
