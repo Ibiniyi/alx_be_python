c24e8cca220f81bfcc9fb04a070ac06ebe0f76da
Timothy <timothyniyi@gmail.com>
2025-09-08 18:31:51 -0700

    py basic operation

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,9 @@
+number1=5
+number2=10
+sum= number1 + number2
+subtraction= number2 -number1
+multiplication = number1 * number2
+
+print(f"the sum of {number1} and {number2} is {sum}" )
+print(f"{number1} subract {number2} is equal {subtraction} ")
+print(f" {number1} times {number2} is equal {multiplication}")
