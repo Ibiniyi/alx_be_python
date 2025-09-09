ed6cea71ec3528c633cfe35fdf707635dff5865d
Timothy <timothyniyi@gmail.com>
2025-09-08 18:52:16 -0700

    variable change

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -4,6 +4,6 @@ sum= number1 + number2
 subtraction= number2 -number1
 multiplication = number1 * number2
 
-print(f"the sum of {number1} and {number2} is {sum}" )
-print(f"{number1} subract {number2} is equal {subtraction} ")
-print(f" {number1} times {number2} is equal {multiplication}")
+print(f"the sum of {number2} and {number1} is {sum}" )
+print(f"{number2} subract {number1} is equal {subtraction} ")
+print(f" {number2} times {number1 is equal {multiplication}")
