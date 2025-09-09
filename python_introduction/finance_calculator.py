0d5a50b6fa74dc7480495dfca2619e7447b89e4d
Timothy <timothyniyi@gmail.com>
2025-09-09 14:48:46 -0700

    change fcal

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,5 +1,5 @@
-income = float(input("Enter your monthly income"))
-expenses = float(input("Enter your monthly expenses"))
+income = float(input( " Enter your monthly income " ))
+expenses = float(input( " Enter your monthly expenses " ))
 monthly_saving = income - expenses
 annual_interest = 0.05
 projected_saving = monthly_saving * 12 + (monthly_saving * 12 * annual_interest)
