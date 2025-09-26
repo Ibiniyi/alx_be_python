ace9f7d92e0348fa98f961a7b8974d497b1c0b21
Timothy <timothyniyi@gmail.com>
2025-09-25 18:52:13 -0700

    arith

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,9 +1,9 @@
 # arithmetic_operations.py
 
-def perform_operation(num1: float, num2: float, operation: str):
+def perform_operation(num1, num2, operation):
     """
     Performs basic arithmetic operations.
-    
+
     Parameters:
         num1 (float): The first number.
         num2 (float): The second number.
