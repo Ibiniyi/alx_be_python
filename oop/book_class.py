6d533fd75ae0030baf7d498e569674769c206dd6
Timothy <timothyniyi@gmail.com>
2025-10-11 22:35:57 -0700

    book_class

---
+++ oop/book_class.py
@@ -0,0 +1,21 @@
+# book_class.py
+
+class Book:
+    def __init__(self, title: str, author: str, year: int):
+        """Constructor to initialize the book details."""
+        self.title = title
+        self.author = author
+        self.year = year
+
+    def __str__(self):
+        """Return a user-friendly string representation of the book."""
+        return f"{self.title} by {self.author}, published in {self.year}"
+
+    def __repr__(self):
+        """Return a developer-friendly string representation of the book."""
+        return f"Book('{self.title}', '{self.author}', {self.year})"
+
+    def __del__(self):
+        """Destructor that prints a message when the book object is deleted."""
+        print(f"Deleting {self.title}")
+
